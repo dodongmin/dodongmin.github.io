77f1ddb1c395bfe1787ac86b449f78acce1c7e61
dodongmin <97537511+dodongmin@users.noreply.github.com>
2025-09-20 00:26:00 +0900

    second commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   site: "https://dodongmin.github.io",
+  output: 'static',
   integrations: [preact(), icon(), sitemap({
     filter: (page) =>
       !page.includes("/blog/tags") &&
